42b6fbacadbd219cd6020b4137aaed95caa4a08d
Greg Miller <gmille81@jh.edu>
2026-01-23 21:19:30 -0500

    Intermittent commit after adding content to various html pages and main.css

--- module_1/app/__init__.py
+++ module_1/app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
+from app import views
 
-@app.route('/')
-
-def hello():
-    return 'Welcome to my website'
-
-if __name__=='__main__':
-    app.run(host='0.0.0.0', port=8080)
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(views.bp)
+    return app
---
+++ module_1/app/views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint,render_template
+
+bp = Blueprint("views",__name__,template_folder='templates')
+
+# @bp.route('/')
+
+# def home():
+#     return render_template('base.html')
+
+@bp.route("/")
+def home():
+    return render_template("pages/home.html")
+
+@bp.route("/contact")
+def contact():
+    return render_template("pages/contact.html")
+@bp.route("/projects")
+def projects():
+    return render_template("pages/projects.html")
